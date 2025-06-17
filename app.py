408a449b09d5ad44945600f1f93e0677fe2e17ad
Aryan <dhandhukiyaaryan05@gmail.com>
2025-06-17 23:25:06 +0530

    aws static ip for backend deployment

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # FastAPI backend URL
-BACKEND_URL = "http://archibus-ai.ddns.net:8000"
+BACKEND_URL = "http://54.194.75.18:8000"
 
 # Function to check if the backend is running
 def check_backend_health():
